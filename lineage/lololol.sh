8ccb2852626721a89193effb43d218f6990aef6e
cytolytic <71159394+aktham3210@users.noreply.github.com>
2020-10-25 03:26:56 +0200

    Update lololol.sh

--- lineage/lololol.sh
+++ lineage/lololol.sh
@@ -9,6 +9,6 @@ SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
 if [ -f "lineage/out/target/product/a30/recovery.img ]; then
    telegram -M "build has been completed ✅! done in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
-   telegram -f "lineage/our/target/product/a30/recovery.img" "[galaxy A30 Lineage recovery] UNOFFICIAL"
+   telegram -f "lineage/out/target/product/a30/recovery.img" "[galaxy A30 Lineage recovery] UNOFFICIAL"
 else
    telegram -M "build failed❌ took $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
