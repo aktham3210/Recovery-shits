ea565a557e3b36ce4a77d9c850601fa0ef112438
cytolytic <71159394+aktham3210@users.noreply.github.com>
2020-10-30 20:55:19 +0200

    Update hehehe.sh

--- TWRP/hehehe.sh
+++ TWRP/hehehe.sh
@@ -5,8 +5,8 @@ export DISABLE_WEB_PAGE_PREVIEW=true
 telegram -M "starting process! TWRP recovery for the galaxy A30 ♻️"
 SYNC_START=$(date +"%s")
 
-mkdir TWRP && cd TWRP
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+mkdir gay && cd gay
+repo init --depth 1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
 repo sync  --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
 git clone --depth 1 https://github.com/crazo7924/twrp_device_samsung_a30 device/samsung/a30
 
