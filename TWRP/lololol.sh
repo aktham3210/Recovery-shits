e3d9767a7c32b23403d19b395f11067fe89153d7
cytolytic <71159394+cytoo@users.noreply.github.com>
2021-01-23 03:28:50 +0200

    why does this shit even exist?

--- TWRP/lololol.sh
+++ TWRP/lololol.sh
@@ -1,4 +1,4 @@
-#/!bin/bash
+#!/bin/bash
 telegram -M "Started building Galaxy A30 TWRP recovery ♻️"
 SYNC_START=$(date +"%s")
 cd TWRP
@@ -7,7 +7,7 @@ lunch omni_a30-eng
 mka recoveryimage -j8
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
-if [ -f "TWRP/out/target/product/a30/recovery.img ]; then
+if [ -f "TWRP/out/target/product/a30/recovery.img" ]; then
    telegram -M "build has been completed ✅! done in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
    telegram -f "TWRP/out/target/product/a30/recovery.img" "[galaxy A30 Lineage recovery] UNOFFICIAL"
 else
